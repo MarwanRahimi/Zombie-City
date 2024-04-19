000c82df5aace8858fddb3989a13307c61aaff91
xReSHo <145558473+xReSHo@users.noreply.github.com>
2024-04-19 12:25:53 +0300

    worked on instruction panel
    completed the design for the instructions panel for both mouse and keyboard and controller, yet there are some bugs that need to be fixed ex. [when click on the back button using the mouse you cant reopen the panel sometimes].

--- Assets/Scripts/InstructionsScript.cs
+++ Assets/Scripts/InstructionsScript.cs
@@ -5,9 +5,36 @@ using UnityEngine;
 public class InstructionsScript : MonoBehaviour
 {
     public GameObject instructionsCanvas;
+    public GameObject controllerInstructions;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            CloseInstructions();
+        }
+    }
 
     public void OpenInstructions()
     {
         instructionsCanvas.SetActive(true);
     }
+
+    public void CloseInstructions()
+    {
+        instructionsCanvas.SetActive(false);
+    }
+
+    public void SwitchToControllerInstructions()
+    {
+        instructionsCanvas.SetActive(false);
+        controllerInstructions.SetActive(true);
+    }
+
+    public void SwitchToMandKInstructions()
+    {
+        instructionsCanvas.SetActive(true);
+        controllerInstructions.SetActive(false);
+    }
+
 }
